0ded1b6537a27f935ef329f91c9a986e5112df29
Thinker Pal <thinkerpal@gmail.com>
2018-12-26 18:41:01 +0800

    more changes

--- customise.sh
+++ customise.sh
@@ -2,19 +2,13 @@ sudo useradd  -m -s /bin/bash dhsuser
 sudo apt install wget
 wget https://raw.githubusercontent.com/ThinkerPal/dhslinuxconfigure/master/sources.list
 sudo cp ./sources.list /etc/apt/
-
-
-sudo apt update
-sudo apt -y purge libreoffice* gparted gnome-disk-utility
 sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
 sudo rm -r /usr/share/ubuntu-web-launchers/amazon-launcher
-
-sudo apt -y install net-tools  geany g++ python3-pip php apache2 nginx whois openssh-server git fish vim emacs npm vim  tmux libxss1 libappindicator1 libindicator7 libc++-dev 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get -y install google-chrome-stable
+sudo apt -y install net-tools  geany g++ python3-pip php apache2 nginx whois openssh-server git fish vim emacs npm vim  tmux libxss1 libappindicator1 libindicator7 libc++-dev google-chrome-stable
 sudo apt -y upgrade
-sudo apt -y purge thunderbird*
+sudo apt -y purge thunderbird* libreoffice* gparted gnome-disk-utility
 
 
