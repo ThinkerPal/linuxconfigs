4691c95195fd893f8f9cfedef0cdc1b00a24f124
Thinker Pal <thinkerpal@gmail.com>
2018-12-26 18:34:22 +0800

    should be fine

--- customise.sh
+++ customise.sh
@@ -9,7 +9,7 @@ sudo apt -y purge libreoffice* gparted gnome-disk-utility
 sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
 sudo rm -r /usr/share/ubuntu-web-launchers/amazon-launcher
 
-sudo apt -y install net-tools  geany g++ python3-pip php apache2 nginx whois openssh-server git fish vim emacs npm vim  tmux libxss1 libappindicator1 libindicator7 libc++-dev libstdc++-7dev
+sudo apt -y install net-tools  geany g++ python3-pip php apache2 nginx whois openssh-server git fish vim emacs npm vim  tmux libxss1 libappindicator1 libindicator7 libc++-dev 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
