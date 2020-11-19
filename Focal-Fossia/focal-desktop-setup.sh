39ded85073193d8a5f4ae2c5ff708461e07c3aaf
Thinker Pal <thinkerpal@gmail.com>
2020-11-19 19:15:22 +0800

    Added vscode; updated focal list linking

--- Focal-Fossia/focal-desktop-setup.sh
+++ Focal-Fossia/focal-desktop-setup.sh
@@ -2,13 +2,20 @@
 
 sudo apt install -y wget curl
 
-wget https://raw.githubusercontent.com/ThinkerPal/dhslinuxconfigure/master/Bionic-Beaver/bionic-sources.list
-sudo cp ./sources.list /etc/apt/
+wget https://raw.githubusercontent.com/ThinkerPal/dhslinuxconfigure/master/Focal-Fossia/focal-sources.list
+sudo cp ./focal-sources.list /etc/apt/sources.list
 sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
 sudo rm -r /usr/share/ubuntu-web-launchers/amazon-launcher
+
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
 sudo apt -y install net-tools  geany g++ python3-pip php apache2 nginx whois openssh-server git fish vim emacs npm vim  tmux libxss1 libappindicator1 libindicator7 libc++-dev google-chrome-stable
 sudo apt -y upgrade
 sudo apt -y purge thunderbird* libreoffice* gparted gnome-disk-utility
